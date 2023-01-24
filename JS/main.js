83d1bbbe94bd05e577e4e7c386e6aed4ff60665e
ThomasAnselmetto <t.anselmetto@gmail.com>
2023-01-24 18:20:06 +0100

    ennesimo tentativo fallimentare

--- JS/main.js
+++ JS/main.js
@@ -1,51 +1,22 @@
-let km_da_percorrere = document.getElementById("journey-km");
-let range_of_age = document.getElementById("age-range");
+console.log(`inizio js`);
+
+const nameInput = document.getElementById("input-nome").value;
+
+const nameTitle = document.getElementById("titolo-nome");
+// const kmInput = document.getElementById("journey-km").value;
+const button = document.getElementById("bottone");
+
+// const costoAlKm = 0.21;
+// const costoTicket = (costoAlKm * kmInput);
+// console.log(costoTicket);
+
+button.addEventListener("click", function () {
+    console.log(nameInput)
+
+
+});
 
 
 
 
 
-
-
-
-
-
-
-let KmPercorsi = prompt("quanti chilometri percorrerai?");
-
-console.log(KmPercorsi);
-console.log(typeof KmPercorsi);
-
-//chiedere l'eta' del passeggero
-
-
-let ClientAge = parseFloat(prompt("quanti anni hai?"));
-
-console.log(ClientAge);
-console.log(typeof ClientAge);
-
-//calcolo del prezzo in base ai km
-
-const CostoAlKm = 0.21;
-const EuroPerTratta = parseFloat(KmPercorsi * CostoAlKm);
-
-
-
-//creazione di un if con le fasce d'eta'
-let ScontoMinorenni = ((EuroPerTratta * 20) / 100);
-
-let ScontoAnziani = ((EuroPerTratta * 40) / 100);
-
-let BigliettoGiovani = EuroPerTratta - ScontoMinorenni
-
-let BigliettoAnziani = EuroPerTratta - ScontoAnziani
-
-if (ClientAge < 18) {
-    console.log("Il tuo biglietto avrà un costo di " + BigliettoGiovani.toFixed(2) + "Є");
-}
-else if (ClientAge > 65) {
-    console.log("Il tuo biglietto avrà un costo di " + BigliettoAnziani.toFixed(2) + "Є");
-}
-else {
-    console.log("Il tuo biglietto avrà un costo di " + EuroPerTratta.toFixed(2) + "Є");
-}
