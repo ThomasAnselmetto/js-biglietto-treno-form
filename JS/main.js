4fa2e703972bd731c3cc0bd82d52236eb1819201
ThomasAnselmetto <t.anselmetto@gmail.com>
2023-01-24 19:51:32 +0100

    added select

--- JS/main.js
+++ JS/main.js
@@ -3,7 +3,7 @@ console.log(`inizio js`);
 const nameInput = document.querySelector("#input-nome");
 const prova = document.querySelector("#titolo-nome")
 const kmInput = document.querySelector("#journey-km");
-
+const selectAge = document.querySelector("#age-range");
 const button = document.getElementById("bottone");
 let costoDelBiglietto;
 const costoAlKm = 0.21;
@@ -15,6 +15,10 @@ button.addEventListener("click", function () {
     costoDelBiglietto = kmInput.value * costoAlKm;
     console.log(kmInput.value * costoAlKm);
     prova.innerHTML = costoDelBiglietto;
+
+
+
+
 
 });
 
