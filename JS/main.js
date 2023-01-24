8cb0ea1346841e9aa356c5b18c1325f55fce6f2c
ThomasAnselmetto <t.anselmetto@gmail.com>
2023-01-24 19:26:09 +0100

    added calcolo costo biglietto

--- JS/main.js
+++ JS/main.js
@@ -1,18 +1,20 @@
 console.log(`inizio js`);
 
 const nameInput = document.querySelector("#input-nome");
-
+const prova = document.querySelector("#titolo-nome")
 const kmInput = document.querySelector("#journey-km");
 
 const button = document.getElementById("bottone");
-
+let costoDelBiglietto;
 const costoAlKm = 0.21;
-const costoTicket = (costoAlKm * kmInput);
-console.log(costoTicket);
+console.log(costoAlKm);
 
 button.addEventListener("click", function () {
-    console.log(nameInput.value)
-
+    console.log(nameInput.value);
+    console.log(kmInput.value);
+    costoDelBiglietto = kmInput.value * costoAlKm;
+    console.log(kmInput.value * costoAlKm);
+    prova.innerHTML = costoDelBiglietto;
 
 });
 
